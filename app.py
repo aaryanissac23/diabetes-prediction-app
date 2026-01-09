8d286246132f52ad9ff85ff9cf73aea31c770291
aaryanissac23 <aaryan.issac23@gmail.com>
2026-01-10 00:50:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import streamlit as st
 import pickle
 import numpy as np
 
+# Load model & scaler
 model = pickle.load(open("diabetes_model.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
 
-st.title("🩺 Diabetes Prediction System")
+st.title("🩺 Diabetes Prediction App")
 
 preg = st.number_input("Pregnancies", min_value=0)
 glucose = st.number_input("Glucose Level", min_value=0)
@@ -16,10 +18,12 @@ dpf = st.number_input("Diabetes Pedigree Function", min_value=0.0)
 age = st.number_input("Age", min_value=1)
 
 if st.button("Predict"):
-    data = np.array([[preg, glucose, bp, skin, insulin, bmi, dpf, age]])
-    result = model.predict(data)
+    input_data = np.array([[preg, glucose, bp, skin, insulin, bmi, dpf, age]])
+    std_data = scaler.transform(input_data)
+    prediction = model.predict(std_data)
 
-    if result[0] == 1:
+    if prediction[0] == 1:
         st.error("⚠️ Person is Diabetic")
     else:
         st.success("✅ Person is NOT Diabetic")
+
